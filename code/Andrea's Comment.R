47ea8ee9a17cf772aa5dd892f5ad9e6bafe1e9da
April Martinig <50909009+martinig@users.noreply.github.com>
2024-02-20 11:38:55 -0700

    edits to reduced sampling

--- code/Andrea's Comment.R
+++ code/Andrea's Comment.R
@@ -1,9 +1,13 @@
 #Andrea's Comment
-#Last edited Feb 19, 2024 by J. I. Sanders
+#Last edited Feb 20, 2024 by A. R. Martinig
 
 # Load packages
 library(purrr)
 library(data.table)
+
+################################################
+#here we create a fake dataset that is ~ similar to the real axy data to test our code on
+################################################
 
 # Create a fake dataset to test the program with
 axy_test <- axy %>%
@@ -42,102 +46,56 @@ result
 
 result %>% filter(squirrel_id== 12678)  %>% arrange(timestamp)
 
+
+################################################
+#this is where we have created the functions to organize the data in a format that will allow us to pull out 7 minutes of data consecutively or randomly
+################################################
+
+
+
 #now that the FAKE data is in a format that is similar to what the axy data will actually be in, we can begin to try to sample the data accordingly
 
 #STEP ONE:
 #keep records for every 30 seconds
 
 keep_30<-result%>%
+#Emily: unhashtag the below script with the real data
+	#filter(!tod=="night", #remove night axy data because focals are never at night 
+	#axy_month %in% c(5:9)) %>% #keep only records from the same months as focals (May through September)
 	group_by(squirrel_id) %>%
   	slice(seq(1, n(), by = 30)) %>%
-  	arrange(squirrel_id, timestamp)
+  	arrange(squirrel_id, timestamp) 
 
 keep_30
 
 keep_30 %>% filter(squirrel_id== 12678)
 
 
-#next steps
 
-#STEP TWO A:
+#STEP TWO:
 #how to have it sample a set amount of time (e.g., 7 minutes of the whole day) - here it could be 1 minute of the "whole (2 minute)" day = continous/consecutive time 
 
-# Function to randomly select one minute of data
-consecutive_minute <- function(x) {
-  n <- nrow(x)
-  if (n < 2) return(NULL)
-  
-  start_index <- sample(1:(n-1), 1)
-  selected_indices <- start_index:(start_index + 1)
-  return(x[selected_indices, , drop = FALSE])
-}
+# Function to consecutively select one minute of data
+consecutive_sample <- keep_30 %>%
+	  arrange(squirrel_id, timestamp) %>%
+	  group_by(squirrel_id) %>% #add in axy_date when we have more data
+	
+	#### Emily: THIS SHOULD BE 1:14 for 7 minutes of data
+	
+	slice(1:2) #keep the first two rows (this means 1 minute of data) # If you want to specify a starting row other than the first row, you can use slice() accordingly. # For example, to start from the third row for each squirrel_id: # slice(3:4)
 
-# Apply the function to each squirrel
-consecutive_minute <- keep_30 %>%
-  group_split(squirrel_id) %>%
-  map(~ consecutive_minute(.x))
+consecutive_sample %>% filter(squirrel_id== 12678)  %>% arrange(timestamp)
 
-# Convert the list of data.frames to a single data.frame
-result2a <- bind_rows(consecutive_minute)
-
-# Print the result
-result2a
-
-result2a %>% filter(squirrel_id== 12678)  %>% arrange(timestamp)
-
-#STEP TWO B:
-#How to have it sample a set amount of time during a specific time of day (e.g., 7 minutes starting at dawn)
- 
-# Function to randomly select one minute of data from one tod variable
- 
-#Select which tod you want to sample
-sample_period = "night"
-
-window_minute <- function(x) {
-  x_window <- x %>% filter(tod == sample_period)
-  n <- nrow(x_window)
-  if (n < 2) return(NULL)
-  
-  start_index <- sample(1:(n-1), 1)
-  selected_indices <- start_index:(start_index + 1)
-  return(x_window[selected_indices, , drop = FALSE])
-}
-
-# Apply the function to each squirrel
-window_minute <- keep_30 %>%
-  group_split(squirrel_id) %>%
-  map(~ window_minute(.x))
-
-# Convert the list of data.frames to a single data.frame
-result2b <- bind_rows(window_minute)
-
-# Print the result
-result2b
-
-result2b %>% filter(squirrel_id== 12678) %>% arrange(timestamp)
 
 #STEP THREE:
 #have it sample a random set amount of time (e.g., 7 minutes randomly spread out across the whole day) - here it could be 1 minute of the "whole (2 minute)" day = noncontinous/nonconsecutive time
 
-# Function to randomly select one minute of data from a two-minute sample
-random_one_minute <- function(x) {
-  n <- nrow(x)
-  if (n < 2) return(NULL)  # Need at least 2 records (1 minute) for sampling
-  
-  selected_indices <- sample (1:n, 2) # Select 2 random samples (one minute total)
-  return(x[selected_indices, , drop = FALSE])
-}
+# Function to randomly select one minute of data
+random_sample <-  keep_30 %>%
+	 group_by(squirrel_id) %>% #add in axy_date when we have more data
 
-# Apply the function to each squirrel
-random_one_minute_result <- keep_30 %>%
-  group_split(squirrel_id) %>%
-  map(~ random_one_minute(.x))
+	#### Emily: THIS SHOULD BE size=14 for 7 minutes of data
 
-# Convert the list of data.frames to a single data.frame
-result3 <- bind_rows(random_one_minute_result)
+	sample_n(size=2) #keep two rows (this means 1 minute of data)
 
-# Print the result
-result3
-
-# Put random samples in numerical order
-result3 %>% filter(squirrel_id== 12678)  %>% arrange(timestamp)
+random_sample %>% filter(squirrel_id== 12678) %>% arrange(timestamp)
