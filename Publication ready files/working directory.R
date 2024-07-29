46ec50774627804bafb4f589a3091ac8ddc8784c
April Martinig <50909009+martinig@users.noreply.github.com>
2024-07-29 12:06:03 -0600

    updated path

--- Publication ready files/working directory.R
+++ Publication ready files/working directory.R
@@ -1,9 +1,9 @@
 #working directory here
 #original code by A. R. Martinig
-#last edited May 7, 2024 by A. R. Martinig
+#last edited July 29, 2024 by A. R. Martinig
 
 #Delete previous information stored 
 rm(list=ls(all=T))
 
 ##set wd to the folder with all your csv's in it
-setwd("~/Documents/Files/Post-docs/UofC/Jonas (ECOL 530)/axy-assay-comparison/for submission")      
+setwd("~/Documents/Files/Manuscripts/Sanders et al. 2024/axy-assay-comparison/for submission")      
